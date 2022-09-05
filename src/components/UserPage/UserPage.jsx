50ad75b154513dd96a3684af869d03f450159b7e
Kas Eam <KassandraEam@gmail.com>
2022-09-04 20:06:03 -0500

    added a few more options to the drop down

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -28,22 +28,42 @@ function UserPage() {
 
       <p>Select a timezone to convert to</p>
        <select name="selectList" id="selectList" onChange={captureTimeZone}>
+        {/* <option value="UTC+2">GMT+1:00</option>
         <option value="UTC+2">GMT+2:00</option>
-        <option value="UTC-6">GMT-6:00</option>
+        <option value="UTC+2">GMT+3:00</option>
+        <option value="UTC+2">GMT+4:00</option>
+        <option value="UTC+2">GMT+5:00</option>
+        <option value="UTC+2">GMT+6:00</option>
+        <option value="UTC+2">GMT+7:00</option>
+        <option value="UTC+2">GMT+8:00</option>
+        <option value="UTC+2">GMT+9:00</option>
+        <option value="UTC+2">GMT+10:00</option>
+        <option value="UTC+2">GMT+11:00</option> */}
+        <option value="UTC-12">GMT-12:00</option>
+        <option value="UTC-11">GMT-11:00</option>
+        <option value="UTC-10">GMT-10:00</option>
+        <option value="UTC-9">GMT-9:00</option>
+        <option value="UTC-8">GMT-8:00</option>
         <option value="UTC-7">GMT-7:00</option>
+        <option value="UTC-6">GMT-6:00</option>
+        <option value="UTC-5">GMT-5:00</option>
+        <option value="UTC-4">GMT-4:00</option>
+        <option value="UTC-3">GMT-3:00</option>
+        <option value="UTC-2">GMT-2:00</option>
+        <option value="UTC-1">GMT-1:00</option>
       </select>
 
       <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
+      {/* <p>Your ID is: {user.id}</p> */}
       {/* <p>Year: {utcNow.year}</p> */}
       {/* <p>Month: {utcNow.month}</p> */}
       {/* <p>Day: {utcNow.day}</p> */}
-      <p>My Time: {now.hour}:{now.minute}</p>
-      <p>My Time in UTC: {utcNow.hour}:{utcNow.minute}</p>
-      <p>Time zone: {timezone}</p>
+      <p>My Time: {now.hour}:0{now.minute}</p>
+      <p>My Time in UTC: {utcNow.hour}:0{utcNow.minute}</p>
+      {/* <p>Time zone I want to convert to: {timezone}</p> */}
 
-      <p>Time Converted to UTC+2: {convertToUTC5.hour}:{convertToUTC5.minute}</p>
-      {/* <LogOutButton className="btn" /> */}
+      <p>Time Converted to {timezone}: {convertToUTC5.hour}:0{convertToUTC5.minute}</p>
+      <LogOutButton className="btn" />
     </div>
   );
 }
