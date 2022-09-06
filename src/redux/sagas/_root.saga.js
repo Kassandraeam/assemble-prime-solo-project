0ffc783d4f507c03e1eb03a8832d503451186770
Kas Eam <KassandraEam@gmail.com>
2022-09-06 16:06:07 -0500

    Branching is terrible

--- src/redux/sagas/_root.saga.js
+++ src/redux/sagas/_root.saga.js
@@ -1,5 +1,6 @@
 import { all } from 'redux-saga/effects';
 import loginSaga from './login.saga';
+import offSetZoneName from './offSetZoneName.saga';
 import registrationSaga from './registration.saga';
 import userSaga from './user.saga';
 
@@ -15,5 +16,6 @@ export default function* rootSaga() {
     loginSaga(), // login saga is now registered
     registrationSaga(),
     userSaga(),
+    offSetZoneName()
   ]);
 }
