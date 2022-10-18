1d9311de492e1efbc81dc4dc80b76a57c02899c6
Kas Eam <KassandraEam@gmail.com>
2022-10-17 22:10:34 -0500

    Succesfully converted military time to 12 hour time.

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,15 +2,20 @@ import React from 'react';
 import { useEffect } from 'react';
 import { useSelector } from 'react-redux';
 import { useDispatch } from 'react-redux';
+const { DateTime } = require("luxon");
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 import './UserPage.css'
 
 function UserPage() {
 
   const user = useSelector((store) => store.user);
+  const userTimezone = user.timezone;
   const availableTimesSpecificToUser = useSelector((store) => store.availabilityReducer)
   console.log(availableTimesSpecificToUser);
+
+  const [time, setTime] = useState();
 
   const dispatch = useDispatch();
 
@@ -32,8 +37,85 @@ function UserPage() {
   }
 
   const convertTime = (free) => {
-    console.log('ConvertTime Function');
-    console.log('free:', free);
+    console.log(free)
+    let test = DateTime.utc(2022, 1, 1, free.time_id, 0).setZone(userTimezone)
+    switch (test.hour) {
+      case 1:
+        return '1:00am'
+        break;
+      case 2:
+        return '2:00am'
+        break;
+      case 3:
+        return '3:00am'
+        break;
+      case 4:
+        return '4:00am'
+        break;
+      case 5:
+        return '5:00am'
+        break;
+      case 6:
+        return '6:00am'
+        break;
+      case 7:
+        return '7:00am'
+        break;
+      case 8:
+        return '8:00am'
+        break;
+      case 9:
+        return '9:00am'
+        break;
+      case 10:
+        return '10:00am'
+        break;
+      case 11:
+        return '11:00am'
+        break;
+      case 12:
+        return '12:00pm'
+        break;
+      case 13:
+        return '1:00pm'
+        break;
+      case 14:
+        return '2:00pm'
+        break;
+      case 15:
+        return '3:00pm'
+        break;
+      case 16:
+        return '4:00pm'
+        break;
+      case 17:
+        return '5:00pm'
+        break;
+      case 18:
+        return '6:00pm'
+        break;
+      case 19:
+        return '7:00pm'
+        break;
+      case 20:
+        return '8:00pm'
+        break;
+      case 21:
+        return '9:00pm'
+        break;
+      case 22:
+        return '10:00pm'
+        break;
+      case 23:
+        return '11:00pm'
+        break;
+      case 24:
+        return '12:00am'
+        break;
+      default:
+        break;
+    }
+    return test.hour;
   }
 
 
@@ -47,8 +129,9 @@ function UserPage() {
       <div className='ml-12 columns-1'>
         {availableTimesSpecificToUser.map(free => (
           <div className='mr-5 mb-8 columns-1' key={free.id}>
-            {convertTime(free)}
-            <p className='text-[20px]'>TIME FREE: {free.day} AT {free.time_id} UTC</p>
+            {/* {convertTime(free)} */}
+            {/* <p>UTC: {free.time_id}</p> */}
+            <p className='text-[20px]'>TIME FREE: {free.day} AT {convertTime(free)}</p>
             <div className='mt-[25px]'>
               <Button variant="contained" className='mt-5' onClick={() => handleDelete(free.id)}>DELETE</Button>
             </div>
@@ -60,14 +143,4 @@ function UserPage() {
   );
 }
 
-// this allows us to use <App /> in index.js
 export default UserPage;
-
-/* 
-{genres.map(genre => (
-                    <div key={genre.name}>
-                        <div className="card-text">{genre.name}</div>
-                    </div>
-                ))}
-
-*/
